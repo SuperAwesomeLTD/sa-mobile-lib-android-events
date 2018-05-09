389b689c88b11643f6273bdf7fea3223a236efd0
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-05-09 17:18:15 +0100

    Update

--- saevents/src/test/java/tv/superawesome/lib/saevents/mocks/server/ResponseFactory.java
+++ saevents/src/test/java/tv/superawesome/lib/saevents/mocks/server/ResponseFactory.java
@@ -2,7 +2,6 @@ package tv.superawesome.lib.saevents.mocks.server;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.SocketPolicy;
-import tv.superawesome.lib.saevents.testutils.ResourceReader;
 
 /**
  * Created by gabriel.coman on 09/05/2018.
@@ -11,8 +10,7 @@ import tv.superawesome.lib.saevents.testutils.ResourceReader;
 public class ResponseFactory {
 
     public static MockResponse successResponse() {
-        String responseBody = ResourceReader.readString("mock_event_success_response.json");
-        return new MockResponse().setBody(responseBody).setResponseCode(200);
+        return new MockResponse().setBody("{}").setResponseCode(200);
     }
 
     public static MockResponse timeoutResponse () {
